0c133c1c957f576f3f77a275689ee09f4ac4138d
Will Reiske <wreiske@mieweb.com>
2019-01-04 21:32:37 -0500

    Git doesn't include empty directories... so let's make them!

--- files/scripts/do-magic.sh
+++ files/scripts/do-magic.sh
@@ -1,3 +1,8 @@
+echo "Make directories to make sure they are there..."
+mkdir -p /etc/ccda/files/validator_configuration/vocabulary/valueset_repository/VSAC
+mkdir -p /etc/ccda/files/validator_configuration/vocabulary/code_repository
+mkdir -p /etc/ccda/files/validator_configuration/scenarios_directory
+
 echo "Copying validator-api xlsx's..."
 cp /etc/submodules/code-validator-api/codevalidator-api/docs/*.xlsx /etc/ccda/files/validator_configuration/vocabulary/valueset_repository/VSAC/
 cp /etc/submodules/code-validator-api/codevalidator-api/docs/ValueSetsHandCreatedbySITE/*.xlsx /etc/ccda/files/validator_configuration/vocabulary/valueset_repository/VSAC/
