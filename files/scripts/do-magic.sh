a34e09a8b2cc289b05127558136dd034613bc65c
Will Reiske <wreiske@mieweb.com>
2019-01-07 15:21:47 -0500

    Added download for ccda-validator war file

--- files/scripts/do-magic.sh
+++ files/scripts/do-magic.sh
@@ -10,5 +10,6 @@ cp /etc/submodules/code-validator-api/codevalidator-api/docs/ValueSetsHandCreate
 ### Removing this file fixes an error 02:11:59,585 ERROR [VocabularyLoadRunner:107] Failed to load configured vocabulary directory.
 rm /etc/ccda/files/validator_configuration/vocabulary/valueset_repository/VSAC/ValueSet_format_Template.xlsx
 
-
+echo "Downloading reference-cdda-validator war file..."
+wget https://github.com/siteadmin/reference-ccda-validator/releases/download/1.0.37/referenceccdaservice.war -O /var/lib/tomcat/webapps/referenceccdaservice.war
 echo "Done!"
